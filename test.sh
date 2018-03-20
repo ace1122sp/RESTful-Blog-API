0ca4d6b1c7c37f661c86022d162e4fac2e852d7b
ace1122sp <ace11.space22@gmail.com>
2018-03-20 17:11:01 +0100

    updated tests

--- test.sh
+++ test.sh
@@ -9,3 +9,22 @@ curl "http://localhost:3000/posts"
 
 # deletes post data at specific id
 curl -X DELETE "http://localhost:3000/posts/0"
+
+
+
+
+# posts comments data
+curl -H "Content-Type: application/json" -X POST -d '{"name": "Top 10 ES6 Features", "url": "http://webapplog.com/es6", "text": ""}' "http://localhost:3000/posts"
+curl -H "Content-Type: application/json" -X POST -d '{"text": "some comment"}' "http://localhost:3000/posts/0/comments"
+curl "http://localhost:3000/posts/0/comments"
+
+# updates comment data at specific id
+curl -H "Content-Type: application/json" -X PUT -d '{"text": "updated comment"}' "http://localhost:3000/posts/0/comments/0"
+
+# gets comment data
+curl "http://localhost:3000/posts/0/comments"
+
+# deletes comment data at specific id
+curl -X DELETE "http://localhost:3000/posts/0/comments/0"
+curl "http://localhost:3000/posts/0/comments"
+curl -X DELETE "http://localhost:3000/posts/0/"
